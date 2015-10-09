a7d5552a6930136772269a51c0ee31ac90d3c403
drmashu <drmashu@gmail.com>
2015-10-10 02:36:52 +0900

    foundationでトップとアイテムページを作成開始

--- src/main/kotlin/io/github/drmashu/koshop/Koshop.kt
+++ src/main/kotlin/io/github/drmashu/koshop/Koshop.kt
@@ -3,8 +3,8 @@ package io.github.drmashu.koshop
 import io.github.drmashu.buri.Buri
 import io.github.drmashu.buri.BuriRunner
 import io.github.drmashu.dikon.Factory
+import io.github.drmashu.dikon.Holder
 import io.github.drmashu.dikon.Injection
-import io.github.drmashu.koshop.model.*
 
 /**
  * Created by drmashu on 2015/10/07.
@@ -16,7 +16,8 @@ fun main(args: Array<String>){
 class Koshop : Buri() {
     override val config: Map<String, Factory<*>>
         get() = mapOf(
-                "/" to Injection(pages.index::class)
+                "title" to Holder("Koshop"),
+                "/" to Injection(pages.index::class),
+                "/item" to Injection(pages.item::class)
         )
-
 }
